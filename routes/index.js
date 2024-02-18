acd16932bb7cab3b04d5afbe8b886cde1589b226
toremann <50668920+toremann@users.noreply.github.com>
2024-02-18 09:34:35 +0100

    s3 test

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,54 @@
+require("dotenv").config()
 const { Router } = require("express");
 const { save } = require("../save_json");
 let favouriteNumber = require("../number.json");
 const add = require("../add");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-  res.json({ status: "success ", message: "Hello world!" });
-});
-
-router.get("/sum/:number1/:number2", (req, res) => {
-  const { number1, number2 } = req.params;
-  if (number1 == null || number2 == null) {
+router.get("/sum/:number1/:number2", async (req, res) => {
+  let my_file = await s3.getObject({
+    Bucket: "cyclic-difficult-deer-pocketbook-eu-west-1",
+    Key: "number.json",
+  }).promise()
+  const favNumber = JSON.parse(my_file.Body)?.favouriteNumber;
+  const {number1, number2} = req.params;
+  if(number1 == null || number2 == null) {
     res.status(400).send("Not provided numbers");
     return;
   }
-  if (isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
+  if(isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
     res.status(400).send("Numbers needs to be integer");
     return;
   }
-  const result = add(
-    favouriteNumber.favouriteNumber,
-    add(parseInt(number1), parseInt(number2))
-  );
+  let result = add(parseInt(number1), parseInt(number2));
+  if(favNumber != null) {
+    result = add(result, favNumber )
+  }
   res.json({
     status: "success",
     result: result,
   });
 });
 
-router.post("/favNumber", (req, res) => {
-  const { number } = req.body;
-  if (number == null) {
+router.post("/favNumber", async (req, res) => {
+  const {number} = req.body;
+  if(number == null ) {
     res.status(400).send("Not provided number");
     return;
   }
-  if (isNaN(parseInt(number))) {
+  if(isNaN(parseInt(number))) {
     res.status(400).send("The number needs to be integer");
     return;
   }
-  favouriteNumber.favouriteNumber = number;
-  save(favouriteNumber);
+  await save({
+    favouriteNumber: number
+  });
   res.json({
     status: "success",
     newFavouriteNumber: number,
-  });
-});
-
-router.delete("/favNumber", (req, res) => {
-  favouriteNumber.favouriteNumber = 0;
-  save(favouriteNumber);
-  res.json({
-    status: "success",
   });
 });
 
--- save_json.js
+++ save_json.js
@@ -1,17 +1,16 @@
+require("dotenv").config()
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
-const save = (favNumber) => {
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3.putObject({
+    Body: JSON.stringify(favNumber, null, 2),
+    Bucket: "cyclic-difficult-deer-pocketbook-eu-west-1",
+    Key: "number.json",
+  }).promise()
 };
 
 module.exports = { save };
